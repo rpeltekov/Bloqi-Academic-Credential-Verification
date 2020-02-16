a3aa258bd6a63cdb3e4803e283b941c677617036
Kristie Huang <kristiehuang@berkeley.edu>
2020-02-16 01:33:11 -0800

    sorry

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import Form from 'react-bootstrap/Form';
 import { Component } from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import { useContext, useRef } from 'react';
-import { UploadsViewStateContext } from '../contexts/viewStateContext';
-
 class App extends Component {
 
     render() {
@@ -51,31 +48,5 @@ class App extends Component {
     }
 }
 
-
-const InputFile = () => {
-    const context = useContext(UploadsViewStateContext);
-    const inputFileRef = useRef < HTMLInputElement > (null);
-
-    const uploadFiles = (files) =>
-        files && context.uploadItems(files);
-
-    const showFileDialog = () =>
-        inputFileRef.current && inputFileRef.current.click();
-
-    return (
-        <div className="input-file">
-            <input
-                style={{display: 'none'}}
-                ref={inputFileRef}
-                type="file"
-                multiple={true}
-                onChange={e => uploadFiles(e.target.files)}
-            />
-            <button onClick={() => showFileDialog()}>
-                Click here to Upload File
-            </button>
-        </div>
-    );
-}
 
 export default App;
